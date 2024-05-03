b6a3ed623224c760bfbcfbfd717fc8fc155f37a1
Motonori IWAMURO <vmi@nifty.com>
2024-05-03 16:48:44 +0900

    tmp: notifyicon(3)

--- SimpleCalendar.Tests/UnitTestDI.cs
+++ SimpleCalendar.Tests/UnitTestDI.cs
@@ -1,10 +1,5 @@
 using CommunityToolkit.Mvvm.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleCalendar.Tests
 {
